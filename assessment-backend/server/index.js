595ac9b03ed98e44508869aa7d94e6b53ca5b63c
Clayton  J. Wilkerson <clay2000lbs@gmail.com>
2021-11-05 15:38:28 -0500

    add new feature complete, 3 more to go

---
+++ assessment-backend/server/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json()); // When we want to be able to accept JSON.
+
+app.get("/api/compliment", (req, res) => {
+  const compliments = ["Gee, you're a smart cookie!",
+					 "Cool shirt!",
+					 "Your Javascript skills are stellar.",
+  ];
+
+  // choose random compliment
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+
+  res.status(200).send(randomCompliment);
+  
+});
+
+app.get("/api/fortunes", (req, res) => {
+  const fortunes  = [
+  "An inch of time is an inch of gold.",
+  "Everyday in your life is a special occasion.",
+  "Failure is the path of least persistence.",
+  "He who expects no gratitude shall never be disappointed.", 
+  "It is better to be an optimist and proven a fool than to be a pessimist and be proven right."]
+  
+  let randomIndex = Math.floor(Math.random() * fortunes.length)
+  let randomFortune = fortunes[randomIndex]
+  
+  res.status(200).send(randomFortune)
+
+})
+
+app.listen(4000, () => console.log("Server running on 4000"));
